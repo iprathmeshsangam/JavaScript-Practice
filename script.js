83b8fd4f32ad386c3a408cbffd9a595106cc4529
prathmesh <iprathmeshsangam@gmail.com>
2024-03-11 08:51:26 +0530

    all javascript practice file

---
+++ script.js
@@ -0,0 +1,32 @@
+var name = "prathmesh";
+let fname = "sangam";
+
+
+
+function myname1() {
+    // console.log(name);
+    // console.log(fname);
+    if(fname !== "sangam"){
+        console.log(fname);
+    }
+}
+
+function myname2(){
+    console.log(name);
+}
+
+
+function myname3(){
+    console.log(name);
+}
+
+
+function myname4(){
+    console.log(name);
+}
+
+
+myname1();
+myname2();
+myname3();
+myname4();
