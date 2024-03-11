53b0d56de8d3f5d527473a990560a16419398803
prathmesh <iprathmeshsangam@gmail.com>
2024-03-11 09:15:19 +0530

    Updated

--- DataTypes.js
+++ DataTypes.js
@@ -3,4 +3,28 @@
 
 //Youtube : https://www.youtube.com/watch?v=-9knnv97wSc&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=5
 
+"use strict";
 
+ // Treat All JS code as newer version as per ECMA script 
+
+// alert("Hello")
+
+
+//Type of Data 
+/*
+//PREMITIVE DATA TYPESs
+    number == > 
+    bigInt
+    string 
+    boolean = > True / false 
+    null = > standalone value
+    undefined => 
+    symbol  => Unique
+
+//NON-PREMITIVE DATA TYPE
+    object = 
+*/
+
+
+console.log(typeof null); // object 
+console.log( typeof undefined); //undefined
