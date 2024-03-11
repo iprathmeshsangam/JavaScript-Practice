3c7e83bdd2388fb12366a961cad7f23a5e3555f7
prathmesh <iprathmeshsangam@gmail.com>
2024-03-11 12:04:58 +0530

    Scope was updated

--- scope.js
+++ scope.js
@@ -9,10 +9,30 @@ if(true){
     console.log("Inner : " , a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
-console.log(c); //value of c is changed 
+// console.log(c); //value of c is changed 
 
 //++++more on scope ++++++++++++
+//nested scope
 
+
+
+
+function one(){
+    const username = "Prathmesh";
+    
+
+    function two(){
+        //age has a blocked scope in function two you can only access age variable inside two() function
+        const age = 27 ;
+
+        console.log(username); // console log
+    }
+    //where as you can access username inside two() as will since the two() is inside a function named one() therefore username has a globle scope
+    two();
+    // console.log(age);
+}
+
+one();
 
