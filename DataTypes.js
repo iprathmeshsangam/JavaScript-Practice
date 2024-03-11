90347308fd71e1fec663bfe861fe3fd49085a53c
prathmesh <iprathmeshsangam@gmail.com>
2024-03-11 09:12:23 +0530

    Data Types

---
+++ DataTypes.js
@@ -0,0 +1,6 @@
+//Data Types in Js
+//Tutorial Git hub Link : https://github.com/hiteshchoudhary/js-hindi-youtube.git
+
+//Youtube : https://www.youtube.com/watch?v=-9knnv97wSc&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=5
+
+
