fb2e318c0508e1d5285642d988f5939a51464b45
prathmesh <iprathmeshsangam@gmail.com>
2024-03-11 21:49:10 +0530

    Added new Files

---
+++ DOM/script.js
@@ -0,0 +1,21 @@
+//Dom
+
+//Document object Model
+//youTube link : https://www.youtube.com/watch?v=Ab6K57WjWTE&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=32
+
+/*
+To get the element { 
+    getElementById
+    getElementByClassName
+    getElementByTagName
+    querySelector
+}
+
+to set the Attribute to an Element {
+    setAttribute("class", "test");
+    getAttribute("id", "class")
+}
+
+
+*/
+
