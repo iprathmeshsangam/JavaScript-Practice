541e273e017236bb3e13953dd98cf2c3f57a5894
prathmesh <iprathmeshsangam@gmail.com>
2024-03-11 10:06:51 +0530

    Added Comparision

---
+++ Comparison.js
@@ -0,0 +1,13 @@
+//comparison in js 
+
+
+let x = true
+let y = false 
+
+console.log(undefined < 0);
+
+//avoid these types of comparisons
+console.log(null >= 0); //false
+console.log(undefined == 0); // true
+console.log(undefined < 0); //false
+console.log(undefined > 0); //false
--- Conversion.js
+++ Conversion.js
@@ -5,27 +5,27 @@
 //NUMBER CONVERSION
 let score = 18; // This is Number
 
-console.log(score);
+// console.log(score);
 
 score = "19abc";
 let scoreIsNumber = Number(score);
 
-console.log(typeof score);
-console.log(scoreIsNumber); // will show NaN 
-console.log(typeof scoreIsNumber); // but type of data will show Number
+// console.log(typeof score);
+// console.log(scoreIsNumber); // will show NaN 
+// console.log(typeof scoreIsNumber); // but type of data will show Number
 
 let score1 = undefined; 
 
-console.log(score1); // undefined 
-console.log(typeof score1); //undefined 
+// console.log(score1); // undefined 
+// console.log(typeof score1); //undefined 
 
 
 //BOOL
 let score3 = 0;
 let changeIntoBoolean = Boolean(score3);
 
-console.log(changeIntoBoolean);
-console.log(typeof changeIntoBoolean); // --- > False
+// console.log(changeIntoBoolean);
+// console.log(typeof changeIntoBoolean); // --- > False
 
 
 
@@ -43,8 +43,8 @@ let someNumber = 33;
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
 
@@ -52,13 +52,13 @@ console.log(typeof stringNumber);
 
 let str = "hello"
 let str1 = " Hitesh"
-console.log(str + str1);
+// console.log(str + str1);
 
-console.log("2" + 1); // 21 
+// console.log("2" + 1); // 21 
 
-console.log("2" + 1 + 1); //211
+// console.log("2" + 1 + 1); //211
 
-console.log(1 + 1 + "2");  //22
+// console.log(1 + 1 + "2");  //22
 
-console.log(3 + 4 + 10 % "x"); //NaN
+// console.log(3 + 4 + 10 % "x"); //NaN
 
