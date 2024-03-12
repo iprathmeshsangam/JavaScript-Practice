978fda0d1d343934ad816f1733180584eecb87e1
prathmesh <iprathmeshsangam@gmail.com>
2024-03-12 19:42:27 +0530

    changes in classes of ballon

--- Projects/Ballon Popper trial/script.js
+++ Projects/Ballon Popper trial/script.js
@@ -4,19 +4,20 @@ let popped = 0;
 
 
 document.addEventListener('mouseover' ,function(e){
-    if(e.target.className === "ballon"){
+     if(e.target.className === "ballon"){
         e.target.style.backgroundColor = "#19172e";
         e.target.textContent = "🎈";
         popped++
         createBallons();
     }
+    
     checkPopped();
 });
 
 
 function checkPopped(){
     let ballongal = document.querySelector(".ballon-Gallery");
-    if(popped == ballongal.lenght || popped ==20){
+    if(popped == ballongal.lenght || popped == 30){
         console.log("All ballons are popped");
         let message = document.querySelector(".no-Ballons");
         message.style.display = "block";
