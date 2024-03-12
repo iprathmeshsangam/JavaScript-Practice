ae78aad628227cc608412af560eac077ee046667
prathmesh <iprathmeshsangam@gmail.com>
2024-03-12 12:55:42 +0530

    added largest

---
+++ Pratice/largest.js
@@ -0,0 +1,17 @@
+ //find the largest number from the given number 
+
+function largest(a,b,c){
+    //find the largest number 
+        let large;
+        
+        if(a > b){
+            large  = a;
+        }else if ( b > c){
+            large  = b;
+        }else { 
+            large = c;
+        }
+        return large;
+}
+
+console.log((10,20,30));
