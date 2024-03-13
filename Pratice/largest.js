d3d87b993c9289426b3d1bee0890ca4ef2ee1cb0
prathmesh <iprathmeshsangam@gmail.com>
2024-03-13 12:32:59 +0530

    edited

--- Pratice/largest.js
+++ Pratice/largest.js
@@ -1,17 +1,23 @@
  //find the largest number from the given number 
 
-function largest(a,b,c){
-    //find the largest number 
-        let large;
+// function largest(a,b,c){
+//     //find the largest number 
+//         let large;
         
-        if(a > b){
-            large  = a;
-        }else if ( b > c){
-            large  = b;
-        }else { 
-            large = c;
-        }
-        return large;
-}
+//         if(a > b){
+//             large  = a;
+//         }else if ( b > c){
+//             large  = b;
+//         }else { 
+//             large = c;
+//         }
+//         return large;
+// }
 
-console.log((10,20,30));
+// console.log((10,20,30));
+
+
+let x = NaN;
+
+console.log()
+
